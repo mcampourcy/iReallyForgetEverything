6c37e0932e435db6bf3a9e1ad6b65cce9ea38ae8
Mireille Campourcy <11388201+mcampourcy@users.noreply.github.com>
2019-07-08 15:46:55 +0200

    Updates

---
+++ precache-manifest.86f820eb460ecc421cd1752068c7c782.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a93f2d13a0dcd550a31d0ed9a7ee0478",
+    "url": "/iReallyForgetEverything/index.html"
+  },
+  {
+    "revision": "2790c44532533aa1f119",
+    "url": "/iReallyForgetEverything/static/js/2.2668f2aa.chunk.js"
+  },
+  {
+    "revision": "b23a472ae488cefa837f",
+    "url": "/iReallyForgetEverything/static/js/main.a8af67a4.chunk.js"
+  },
+  {
+    "revision": "334bc763bb4fce898cba",
+    "url": "/iReallyForgetEverything/static/js/runtime~main.965d3a5e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.04955b1aa4fdf4a5c5d62aeb607b5d19.js"
+  "/iReallyForgetEverything/precache-manifest.86f820eb460ecc421cd1752068c7c782.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/iReallyForgetEverything/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
